a456372e6002de54d8224c2c818dc30bc9e7088c
Mykhailo Balakhon <9mohapx9@gmail.com>
2024-02-26 15:28:44 +0100

    Init version

---
+++ README.md
@@ -0,0 +1,1 @@
+# The MarkDown to HTML parser
---
+++ src/main/java/ua/mibal/Launcher.java
@@ -0,0 +1,11 @@
+package ua.mibal;
+
+/**
+ * @author Mykhailo Balakhon
+ * @link <a href="mailto:9mohapx9@gmail.com">9mohapx9@gmail.com</a>
+ */
+public class Launcher {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
