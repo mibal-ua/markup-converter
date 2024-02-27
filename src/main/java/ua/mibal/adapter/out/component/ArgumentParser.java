d2b5cc1c8c59214d9e89c4accab47bd75949569b
Mykhailo Balakhon <9mohapx9@gmail.com>
2024-02-27 10:31:23 +0100

    Implement `ArgumentParser`

--- src/main/java/ua/mibal/adapter/out/component/ArgumentParser.java
+++ src/main/java/ua/mibal/adapter/out/component/ArgumentParser.java
@@ -25,9 +25,36 @@ import ua.mibal.adapter.out.model.Arguments;
  */
 @Component
 public class ArgumentParser {
+    private static final String KEY_PREFIX = "--";
 
-    // TODO
     public Arguments parse(String[] args) {
-        throw new UnsupportedOperationException("Not implemented");
+        validate(args);
+
+        Arguments arguments = new Arguments();
+        for (int i = 0; i < args.length; i += 2) {
+            String key = getKeyName(args[i]);
+            String value = args[i + 1];
+            arguments.put(key, value);
+        }
+        return arguments;
+    }
+
+    private void validate(String[] args) {
+        if (args.length % 2 != 0) {
+            throw new ArgumentParserException(
+                    "Arguments length must be even number. " +
+                    "Actual is " + args.length
+            );
+        }
+    }
+
+    private String getKeyName(String arg) {
+        if (!arg.startsWith(KEY_PREFIX)) {
+            throw new ArgumentParserException(String.format(
+                    "Key '%s' must starts with '%s'",
+                    arg, KEY_PREFIX
+            ));
+        }
+        return arg.substring(KEY_PREFIX.length());
     }
 }
---
+++ src/test/java/ua/mibal/adapter/out/component/ArgumentParser_UnitTest.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) 2024. mailto:9mohapx9@gmail.com
+ *
+ * The Hotel booking service Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package ua.mibal.adapter.out.component;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ua.mibal.adapter.out.model.Arguments;
+import ua.mibal.test.annotation.UnitTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+/**
+ * @author Mykhailo Balakhon
+ * @link <a href="mailto:9mohapx9@gmail.com">9mohapx9@gmail.com</a>
+ */
+@UnitTest
+class ArgumentParser_UnitTest {
+    private static final String KEY_PREFIX = "--";
+
+    private ArgumentParser parser;
+
+    @BeforeEach
+    void setUp() {
+        parser = new ArgumentParser();
+    }
+
+    @Test
+    void parse() {
+        String key = "key";
+        String val = "val";
+        String[] args = {KEY_PREFIX + key, val};
+
+        Arguments arguments = parser.parse(args);
+
+        assertEquals(val, arguments.get(key));
+    }
+
+    @Test
+    void parse_should_throw_ArgumentParserException_if_args_length_is_not_even() {
+        String[] args = {"first"};
+
+        assertThrows(ArgumentParserException.class,
+                () -> parser.parse(args));
+    }
+
+    @Test
+    void parse_should_throw_ArgumentParserException_if_args_format_is_invalid() {
+        String[] args = {"key_without_prefix", "val"};
+
+        assertThrows(ArgumentParserException.class,
+                () -> parser.parse(args));
+    }
+}
