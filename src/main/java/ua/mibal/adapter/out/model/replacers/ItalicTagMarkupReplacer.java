89315d1ca136526fe71495e394dd3422f7c814c9
Mykhailo Balakhon <9mohapx9@gmail.com>
2024-03-07 19:05:09 +0100

    Fix `ItalicTagMarkupReplacer` validation logic
    Add cases when tag is opened at the end of line and at the middle of the line

--- src/main/java/ua/mibal/adapter/out/model/replacers/ItalicTagMarkupReplacer.java
+++ src/main/java/ua/mibal/adapter/out/model/replacers/ItalicTagMarkupReplacer.java
@@ -3,6 +3,7 @@ package ua.mibal.adapter.out.model.replacers;
 import org.springframework.stereotype.Component;
 import ua.mibal.adapter.out.model.MarkupValidationException;
 
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -38,20 +39,34 @@ public class ItalicTagMarkupReplacer extends RegexpMarkupReplacer {
 
     @Override
     protected void validate(String input) {
-        Pattern pattern = Pattern.compile("_\\w.[^_]+\\b$", MULTILINE);
-        Matcher matcher = pattern.matcher(input);
+        Pattern notClosedAtTheEndPattern = Pattern.compile("_\\w.[^_]+\\b(?!(.*\\w_\\b))", MULTILINE);
+        Pattern notClosedAtTheMiddlePattern = Pattern.compile("_\\w.[^_]+\\b(?=(\\b_\\B([^_><]+)\\B_\\b))", MULTILINE);
 
-        if (matcher.find()) {
-            MarkupValidationException parentException = new MarkupValidationException(
-                    "Exception for markdown '_' tag"
-            );
-            do {
-                String quote = matcher.group();
-                parentException.addSuppressed(new MarkupValidationException(
-                        "Markdown tag is not closed for fragment '" + quote + "'"
-                ));
-            } while (matcher.find());
-            throw parentException;
+        checkForViolation(input, Map.of(
+                notClosedAtTheEndPattern, "Markdown tag is not closed",
+                notClosedAtTheMiddlePattern, "Markdown tag is not closed"
+        ));
+    }
+
+    private void checkForViolation(String input, Map<Pattern, String> patterns) {
+        for (Map.Entry<Pattern, String> entry : patterns.entrySet()) {
+            String message = entry.getValue();
+            Pattern pattern = entry.getKey();
+
+            Matcher matcher = pattern.matcher(input);
+
+            if (matcher.find()) {
+                MarkupValidationException parentException = new MarkupValidationException(
+                        "Exception for markdown '_' tag"
+                );
+                do {
+                    String quote = matcher.group();
+                    parentException.addSuppressed(new MarkupValidationException(
+                            message + " for fragment '" + quote + "'"
+                    ));
+                } while (matcher.find());
+                throw parentException;
+            }
         }
     }
 }
